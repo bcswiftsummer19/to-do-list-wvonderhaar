423affd4086c7e396bebc908fbf5116cae3b35ab
Walker Vonder Haar <vonderhw@bc.edu>
2019-06-11 19:56:18 +0100

    5.3

---
+++ ToDoList/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  ToDoList
+//
+//  Created by Walker Vonder Haar on 6/11/19.
+//  Copyright © 2019 Walker Vonder Haar. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var toDoField: UITextField!
+    var toDoItem: String?
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let toDoItem = toDoItem {
+            toDoField.text = toDoItem
+        }
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "UnwindFromSave" {
+            toDoItem = toDoField.text
+        }
+    }
+
+    @IBAction func cancelPressed(_ sender: UIBarButtonItem) {
+        let isPresentingInAddMode = presentingViewController is UINavigationController
+        if isPresentingInAddMode {
+            dismiss(animated: true, completion: nil)
+        } else {
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
